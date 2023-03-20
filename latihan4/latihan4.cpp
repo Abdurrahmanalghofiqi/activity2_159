dbd6a5b4a11cfb24b9153e7ff96fb5f2ef4e65d8
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2023-03-20 18:45:27 +0700

    menambah < pada line 16 karena disitu kurang 1

--- latihan4/latihan4.cpp
+++ latihan4/latihan4.cpp
@@ -10,3 +10,48 @@ float LuasLingkaran(float r) {
 float LuasSegitiga(float a, float t) {
 	return 0.5 * a * t;
 }
+
+int main()
+{
+	float panjang, lebar, jejari, alas, tinggi;
+	int pilihan;
+
+	do {
+		cout << "\n\n======================" << endl;
+		cout << "======MENU============" << endl;
+		cout << "========================" << endl;
+		cout << "1. Luas Persegi " << endl;
+		cout << "2. Luas Lingkaran " << endl;
+		cout << "3. Luas Segitiga " << endl;
+		cout << "4. Exit" << endl;
+		cout << "pilihan (1/2/3/4)";
+		cin >> pilihan;
+
+		switch (pilihan)
+		{
+		case 1:
+			cout << "Masukan Panjang = ";
+			cin >> panjang;
+			cout << "Masukan Lebar = ";
+			cin >> lebar;
+			cout << "LLuas Persegi Panjang = " << LuasPersegi(panjang, lebar);
+			break;
+		case 2:
+			cout << "Masukan Jari - Jari = ";
+			cin >> jejari;
+			cout << "Luas Lingkaran = " << LuasLingkaran(jejari);
+			break;
+		case 3:
+			cout << "Masukan  Alas = ";
+			cin >> alas;
+			cout << "Masukan Tinggi = ";
+			cin >> tinggi;
+			cout << "Luas Segitiga = " << LuasSegitiga(alas, tinggi);
+			break;
+		case 4:
+			break;
+		default:
+			cout << "PILIHAN SALAH" << endl;
+		}
+	} while (pilihan != 4);
+}
