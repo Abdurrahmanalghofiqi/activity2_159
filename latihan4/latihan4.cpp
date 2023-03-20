dc1d5b7a302fcc7b7a29f2464201283a0e23d96e
Abdurrahmanalghofiqi <abdurrahmanalghofiqi69@gmail.com>
2023-03-20 16:40:16 +0700

    memasukan rumus luas persegi , lingkaran, segitiga

--- latihan4/latihan4.cpp
+++ latihan4/latihan4.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
-using namespace srd;
+using namespace std;
 
-float LuasPersegi
+float LuasPersegi(float p, float l) {
+	return p * l;
+}
+float LuasLingkaran(float r) {
+	return 3.14 * r * r;
+}
+float LuasSegitiga(float a, float t) {
+	return 0.5 * a * t;
+}
